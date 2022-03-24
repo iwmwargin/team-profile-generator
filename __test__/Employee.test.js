ac62ef15ad1ef07da8a510b72796fb059654741e
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-03-24 18:42:13 -0400

    removed src folder and the create file it contained because Im doing the html generating in the index file

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,15 +1,30 @@
 const Employee = require("../lib/Employee");
 
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof(e)).toBe("object");
+});
 
-describe("Manager class", () => {
-    test("create an instance of an employee", () => {
-        const employee = new Employee();
-        expect(employee).toBeInstanceof(Employee);
-    })
-    test("name property returns correct name", () => {
-        const manager = new Manager("Eric");
-        const actualManagersName = manager.name;
-        const expectedManagersName = "Eric";
-        expect(actualManagersName).toBe(expectedManagersName);
-    });
+test("Can set name via constructor arguments", () => {
+  const name = "Alice";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
+
+test("Can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test("Can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.email).toBe(testValue);
+});
+
+test("Can get name via getName()", () => {
+  const testValue = "Alice";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
 });
--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,19 +1,19 @@
 const Manager = require("../lib/Manager");
 
-test("Get school name via constructor", () => {
-  const school = "J&W";
-  const a = new Manager("Eric", 1, "turtle@turtle.com", school);
-  expect(a.school).toBe(school);
+test("Get Office number via constructor", () => {
+  const officeNum = "4";
+  const a = new Manager("Eric", 1, "turtle@turtle.com", "4");
+  expect(a.officeNum).toBe(officeNum);
 });
 
-test("Get School username via getSchool()", () => {
-    const school = "J&W";
-    const a = new Manager("iwmwargin", 1, "scrim@scram.com", school);
-    expect(a.getSchool()).toBe(school);
+test("Get office number via getOffice()", () => {
+    const officeNum = "4";
+    const a = new Manager("iwmwargin", 1, "scrim@scram.com", "4");
+    expect(a.getOffice()).toBe(officeNum);
   });
 
 test("getRole() should return \"Manager\"", () => {
   const manager = "Manager";
-  const a = new Manager("Eric", 1, "test@test.com", "ASU");
-  expect(a.getRole()).toBe("Intern");
+  const a = new Manager("Eric", 1, "test@test.com", "Manager");
+  expect(a.getRole()).toBe("Manager");
 });
