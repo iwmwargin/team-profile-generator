16233495ca901f0bf66fcb7df08ebd0cecb31564
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-03-24 18:53:50 -0400

    added walkthrough video

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const newHTML = require("./src/createHTML");
 const team = [];
 
 const initialQuest = [
